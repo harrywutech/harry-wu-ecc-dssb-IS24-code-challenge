b25d339abc1ecc07c21683b0ba32f8b3fb22e7cd
Harry Wu <harry.wu.tech@gmail.com>
2023-10-09 21:54:18 -0700

    add cors

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const productsRoutes = require("./routes/products-routes");
 const HttpError = require("./models/http-error");
@@ -9,15 +10,12 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH");
-  next();
-});
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept']
+}));
 
 app.use("/api/products", productsRoutes);
 
